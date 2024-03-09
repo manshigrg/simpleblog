c8170d230f72a6d090ad12fde46c4217ffe7c6da
manshigrg <manshigurung824@gmail.com>
2024-03-09 21:00:25 +0545

    update 2.1

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
 #from . import views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('logout/', LogoutView.as_view(), name='logout'),
-
+    path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
 ]
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 #from django.contrib.auth import authenticate, login, logout
 #from django.contrib.auth.decorators import login_required
 #from .forms import EditProfileForm
@@ -13,3 +14,20 @@ class UserRegisterView(generic.CreateView):
 	form_class = SignUpForm
 	template_name = 'registration/register.html'
 	success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+	form_class = EditProfileForm
+	template_name = 'registration/edit_profile.html'
+	success_url = reverse_lazy('home')
+
+	def get_object(self):
+		return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+	form_class = PasswordChangeForm
+	success_url = reverse_lazy('home')
+
+
+
+
