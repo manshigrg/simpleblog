f83aa7491e8672e1f19312cf390e44cbc5d2812d
manshigrg <manshigurung824@gmail.com>
2024-03-11 17:44:52 +0545

    update 2.2

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,6 +109,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
 
 
 # Default primary key field type
