8c4d9fc8da8c9f277f59972445d1c3a38b8c0d34
manshigrg <manshigurung824@gmail.com>
2024-03-11 23:43:18 +0545

    update 2.4

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
+    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),   
 ]
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from .forms import SignUpForm, EditProfileForm, ProfilePageForm
 from theblog.models import Profile
 #from django.contrib.auth import authenticate, login, logout
 #from django.contrib.auth.decorators import login_required
@@ -12,13 +12,23 @@ from theblog.models import Profile
 #from django.contrib import messages
 #from .forms import RegisterUserForm
 
+class CreateProfilePageView(CreateView):
+	model = Profile
+	form_class = ProfilePageForm
+	template_name = 'registration/create_user_profile_page.html'
+	#fields = '__all__'
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super().form_valid(form)
+
+
 class EditProfilePageView(generic.UpdateView):
 	model = Profile
 	template_name = 'registration/edit_profile_page.html'
 	fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url']
 
 	success_url = reverse_lazy('home')
-
 
 class ShowProfilePageView(DetailView):
 	model = Profile
